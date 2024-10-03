dde542e44de246ce849c1c1e8d90110b3e9ed1ab
PrinceMuichkine <babacar.diop.bba@edhec.com>
2024-10-03 19:30:09 +0000

    mesure de securite draconnienne

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { terser } from 'rollup-plugin-terser';
 import { visualizer } from 'rollup-plugin-visualizer';
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 export default defineConfig({
   plugins: [
@@ -15,6 +16,67 @@ export default defineConfig({
     visualizer({
       filename: 'bundle-analysis.html',
       open: true,
+    }),
+    obfuscatorPlugin({
+      include: ["src/**/*.ts", "src/**/*.tsx"],
+      exclude: [/node_modules/],
+      apply: "build",
+      debugger: true,
+      options: {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 0.75,
+        deadCodeInjection: true,
+        deadCodeInjectionThreshold: 0.4,
+        debugProtection: false,
+        debugProtectionInterval: 0,
+        disableConsoleOutput: true,
+        domainLock: [],
+        domainLockRedirectUrl: 'about:blank',
+        forceTransformStrings: [],
+        identifierNamesCache: null,
+        identifierNamesGenerator: 'hexadecimal',
+        identifiersDictionary: [],
+        identifiersPrefix: '',
+        ignoreImports: false,
+        inputFileName: '',
+        log: false,
+        numbersToExpressions: false,
+        optionsPreset: 'default',
+        renameGlobals: true,
+        renameProperties: false,
+        renamePropertiesMode: 'safe',
+        reservedNames: [],
+        reservedStrings: [],
+        seed: 0,
+        selfDefending: true,
+        simplify: true,
+        sourceMap: false,
+        sourceMapBaseUrl: '',
+        sourceMapFileName: '',
+        sourceMapMode: 'separate',
+        sourceMapSourcesMode: 'sources-content',
+        splitStrings: false,
+        splitStringsChunkLength: 10,
+        stringArray: true,
+        stringArrayCallsTransform: true,
+        stringArrayCallsTransformThreshold: 0.5,
+        stringArrayEncoding: [],
+        stringArrayIndexesType: [
+          'hexadecimal-number'
+        ],
+        stringArrayIndexShift: true,
+        stringArrayRotate: true,
+        stringArrayShuffle: true,
+        stringArrayWrappersCount: 1,
+        stringArrayWrappersChainedCalls: true,
+        stringArrayWrappersParametersMaxCount: 2,
+        stringArrayWrappersType: 'variable',
+        stringArrayThreshold: 0.75,
+        target: 'browser',
+        transformObjectKeys: false,
+        unicodeEscapeSequence: false
+      },
     }),
   ],
   build: {
