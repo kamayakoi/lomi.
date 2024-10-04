b2e8a206ad3c7e7148f0d5c8d43f655bf8a6bb83
PrinceMuichkine <babacar.diop.bba@edhec.com>
2024-10-04 03:25:12 +0000

    new loader

--- src/components/dashboard/loader.tsx
+++ src/components/dashboard/loader.tsx
@@ -2,8 +2,8 @@ import { Loader2 } from "lucide-react"
 
 export default function LoadingButton() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex items-center gap-2 text-gray-700">
+    <div className="flex h-screen items-center justify-center bg-white dark:bg-gray-900">
+      <div className="flex items-center gap-2 text-gray-700 dark:text-white">
         <Loader2 className="h-5 w-5 animate-spin" />
         <span>Loading</span>
       </div>
