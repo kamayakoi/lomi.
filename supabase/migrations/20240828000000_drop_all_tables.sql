b129827b8265c3c9d30fd26c43df7d59e50767e0
PrinceMuichkine <babacar.diop.bba@edhec.com>
2025-02-15 10:51:07 +0000

    Stop tracking supabase

--- supabase/migrations/20240828000000_drop_all_tables.sql
+++
@@ -1,9 +0,0 @@
--- Drop all tables
-DROP SCHEMA public CASCADE;
-
--- Recreate the public schema
-CREATE SCHEMA public;
-
--- Grant privileges
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO public;
