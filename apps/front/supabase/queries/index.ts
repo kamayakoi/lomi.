f83a842a87167c55343fe6db8aa8c642c11767cb
PrinceMuichkine <babacar.diop.bba@edhec.com>
2025-03-13 12:25:39 +0000

    chore: update gitignore to exclude supabase/queries folder and env.example

--- apps/front/supabase/queries/index.ts
+++
@@ -1,9 +0,0 @@
-// Core exports
-export * from './queries/customers'
-export * from './queries/transactions'
-export * from './queries/products'
-export * from './queries/subscriptions'
-export * from './queries/organizations'
-
-// Cached queries exports
-export * from './cached-queries'
